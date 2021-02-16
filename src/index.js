4c0ac8f9eec057afb57b0e887fe4b543cf973b5b
hklee <hk@ccgolftour.co.kr>
2021-02-16 23:57:12 +0900

    무엇을 수정했는지 모름

--- src/index.js
+++ src/index.js
@@ -18,9 +18,11 @@ const { jwtMiddleware } = require('lib/token');
  
 mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
 mongoose.set('useFindAndModify', false);
-mongoose.connect(process.env.MONGO_URI1,  
-{useNewUrlParser: true, useUnifiedTopology: true,
-    retryWrites: false})
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  retryWrites: false
+})
 .then(response => {
     console.log('mongodb 접속 성공적~');
   })
